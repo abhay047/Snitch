f4e92330fea2b9152bd30acd77d7fdae3aef06f1
Abhay <bklucky1976@gmail.com>
2026-09-21 14:26:00 +0530

    feat: add Google authentication button and update environment configuration

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -9,6 +9,7 @@ import {
     googleCallback,
 } from "../controllers/auth.controller.js";
 import passport from "passport";
+import { config } from "../config/config.js";
 
 const router = Router();
 
@@ -23,7 +24,7 @@ router.get(
 
 router.get(
     "/google/callback",
-    passport.authenticate("google", { session: false, failureRedirect: "http://localhost:5173/login" }),
+    passport.authenticate("google", { session: false, failureRedirect: config.NODE_ENV === "development" ? "http://localhost:5173/login" : "/login" }),
     googleCallback,
 );
 
